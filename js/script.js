880365e8a837fe5e36aff36e471a4bcf83c420d4
Dmytro Demchenko <dseg9@mail.ru>
2021-04-02 15:20:56 +0300

    add array, output correctAnswer

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 let num;
 let score;
 const question1 = {
-
+    question: '2 + 2 = ?',
+    answer: '',
+    correctAnswer: ''
 }
 const question2 = {
-
+    question: '5 + 9 = ?',
+    answer: '',
+    correctAnswer: ''
 }
 const question3 = {
-
+    question: 'a - b = ?',
+    answer: '',
+    correctAnswer: 'c'
 }
 const question4 = {
-
+    question: '20 * 5 = ?',
+    answer: '',
+    correctAnswer: ''
 }
+const q = [
+    question1, question2, question3, question4
+]
+q.forEach((item) => {
+    if (item.correctAnswer === 'c') {
+        console.log(item.correctAnswer);
+    }
+})
