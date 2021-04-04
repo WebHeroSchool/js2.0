e96ab70ea4e5641b0d5a94c97ce18519f98b9f2c
Dmytro Demchenko <dseg9@mail.ru>
2021-04-04 14:52:05 +0300

    add function

--- js/script.js
+++ js/script.js
@@ -2,29 +2,40 @@ let num;
 let score;
 const question1 = {
     question: '2 + 2 = ?',
-    answer: '',
-    correctAnswer: ''
+    answer: (prompt('1. 2 + 2 = ?')),
+    correctAnswer: '4'
 }
 const question2 = {
     question: '5 + 9 = ?',
-    answer: '',
-    correctAnswer: ''
+    answer: (prompt('1. 5 + 9 = ?')),
+    correctAnswer: '14'
 }
 const question3 = {
     question: 'a - b = ?',
-    answer: '',
+    answer: (prompt('2. a - b = ?')),
     correctAnswer: 'c'
 }
 const question4 = {
     question: '20 * 5 = ?',
-    answer: '',
-    correctAnswer: ''
+    answer: (prompt('20 * 5 = ?')),
+    correctAnswer: '100'
 }
 const q = [
-    question1, question2, question3, question4
-]
-q.forEach((item) => {
-    if (item.correctAnswer === 'c') {
-        console.log(item.correctAnswer);
-    }
-})
+        question1, question2, question3, question4
+    ]
+    // q.forEach((item) => {
+    //     if (item.correctAnswer === 'c') {
+    //         console.log(item.correctAnswer);
+    //     }
+    // })
+
+buildQuiz = q => {
+    q.forEach((item) => {
+        if (item.answer === item.correctAnswer) {
+            alert('Верно');
+        } else {
+            console.log('Не верно');
+        }
+    })
+}
+buildQuiz(q);
